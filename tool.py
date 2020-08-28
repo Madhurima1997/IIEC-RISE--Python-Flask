0e2a018ef149ddba7a01a176a636b041a3a1016d
Madhurima Chakraborty <61430343+Madhurima1997@users.noreply.github.com>
2020-08-28 20:39:31 +0530

    Update tool.py

--- tool.py
+++ tool.py
@@ -4,7 +4,7 @@ print("Hello User! How can I help you?")
 pyttsx3.speak("Hello User! How can I help you?")
 while(True):
     p=input("User: ")
-    if(("chrome" in p) or ("internet" in p) or("net" in p) or ("Chrome" in p) or ("Internet" in p) or("Net" in p)):
+    if(("chrome" in p) or ("internet" in p) or("net" in p) or ("Chrome" in p) or ("Internet" in p) or("Net" in p) or ("browse" in p) or ("Browse" in p) or ("browsing" in p) or ("Browsing" in p) or ("surf" in p) or ("Surf" in p) or ("surfing" in p) or ("Surfing" in p)):
         if(("don't" in p) or ("dont" in p) or ("do not" in p) or ("Don't" in p) or ("Dont" in p)):
             print("OK! What else can I do for you?") 
             pyttsx3.speak("OK! What else can I do for you?")
@@ -15,7 +15,10 @@ while(True):
             print("What else can I do for you?") 
             pyttsx3.speak("What else can I do for you?")
     elif(("note" in p) or ("Note" in p) or ("document" in p) or ("Document" in p) or ("text" in p) or ("Text" in p) or ("jot" in p) or ("write" in p) or ("Write" in p) or ("notepad" in p) or ("Notepad" in p) or ("word" in p) or ("editor" in p) or ("Editor" in p) or ("msword" in p) or ("MsWord" in p) or ("MSWord" in p)):
-        if(("notepad" in p)or ("Notepad" in p)):
+        if(("don't" in p) or ("dont" in p) or ("do not" in p) or ("Don't" in p) or ("Dont" in p)):
+            print("OK! What else can I do for you?") 
+            pyttsx3.speak("OK! What else can I do for you?")
+        elif(("notepad" in p)or ("Notepad" in p)):
             print("Launching notepad for you!")
             pyttsx3.speak("Launching notepad for you")
             os.system("notepad")
@@ -27,9 +30,6 @@ while(True):
             os.system("start winword")
             print("What else can I do for you?") 
             pyttsx3.speak("What else can I do for you?")
-        elif(("don't" in p) or ("dont" in p) or ("do not" in p) or ("Don't" in p) or ("Dont" in p)):
-            print("OK! What else can I do for you?") 
-            pyttsx3.speak("OK! What else can I do for you?")
         else:
             print("Which editor do you want to use? MSWord or Notepad ?")
             pyttsx3.speak("Which editor do you want to use? MSWord or Notepad ?")
@@ -48,9 +48,12 @@ while(True):
                 pyttsx3.speak("What else can I do for you?")
             else:
                 print("Sorry User! I do not support this editor")
-                pyttsx3.speak()
-    elif((("don't" in p) or ("do not" in p))):
-        print("OK User! What else can I do for you?")
+                pyttsx3.speak("Sorry User! I do not support this editor")
+                print("What else can I do for you?")
+                pyttsx3.speak("What else can I do for you?")
+    elif(("Thanks" in p) or ("Thank" in p) or ("thanks" in p) or ("thank" in p)):
+        print("Glad to help you!")
+        pyttsx3.speak("Glad to help you!")
     elif(("mails" in p) or ("email" in p) or ("emails" in p)):
         os.system("start chrome gmail.com")
     elif(("stop" in p) or ("exit" in p) or ("Exit" in p) or ("Stop" in p) or ("will be all" in p) or ("That's all" in p) or ("that's all" in p) or ("thats all" in p)):
